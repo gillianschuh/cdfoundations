eaab79247bcb43e92bdec8b0a36787a06d2b13f1
gillianschuh <schug895@newschool.edu>
2022-04-04 17:31:12 -0400

    changed files

---
+++ adlib/myscript.js
@@ -0,0 +1,29 @@
+console.log(document.getElementById("field01").value);
+document.getElementById("myButton").onclick = function () {
+  document.getElementById("word01").innerHTML = document.getElementById("field01").value;
+  document.getElementById("story").style.display = "block";
+}
+
+console.log(document.getElementById("field02").value);
+document.getElementById("myButton").onclick = function () {
+  document.getElementById("word02").innerHTML = document.getElementById("field02").value;
+  document.getElementById("story").style.display = "block";
+}
+
+console.log(document.getElementById("field03").value);
+document.getElementById("myButton").onclick = function () {
+  document.getElementById("word03").innerHTML = document.getElementById("field03").value;
+  document.getElementById("story").style.display = "block";
+}
+
+console.log(document.getElementById("field04").value);
+document.getElementById("myButton").onclick = function () {
+  document.getElementById("word04").innerHTML = document.getElementById("field04").value;
+  document.getElementById("story").style.display = "block";
+}
+
+console.log(document.getElementById("field05").value);
+document.getElementById("myButton").onclick = function () {
+  document.getElementById("word05").innerHTML = document.getElementById("field05").value;
+  document.getElementById("story").style.display = "block";
+}
