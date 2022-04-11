6f68385e4b99c22ce1379daacb0e1ae135968f80
gillianschuh <schug895@newschool.edu>
2022-04-11 17:50:18 -0400

    confirm changes

--- adlib/myscript.js
+++ adlib/myscript.js
@@ -1,29 +1,16 @@
 console.log(document.getElementById("field01").value);
 document.getElementById("myButton").onclick = function () {
   document.getElementById("word01").innerHTML = document.getElementById("field01").value;
-  document.getElementById("story").style.display = "block";
-}
-
-console.log(document.getElementById("field02").value);
-document.getElementById("myButton").onclick = function () {
   document.getElementById("word02").innerHTML = document.getElementById("field02").value;
-  document.getElementById("story").style.display = "block";
-}
-
-console.log(document.getElementById("field03").value);
-document.getElementById("myButton").onclick = function () {
   document.getElementById("word03").innerHTML = document.getElementById("field03").value;
-  document.getElementById("story").style.display = "block";
-}
-
-console.log(document.getElementById("field04").value);
-document.getElementById("myButton").onclick = function () {
   document.getElementById("word04").innerHTML = document.getElementById("field04").value;
-  document.getElementById("story").style.display = "block";
-}
-
-console.log(document.getElementById("field05").value);
-document.getElementById("myButton").onclick = function () {
   document.getElementById("word05").innerHTML = document.getElementById("field05").value;
+  document.getElementById("word06").innerHTML = document.getElementById("field06").value;
+  document.getElementById("word07").innerHTML = document.getElementById("field07").value;
+  document.getElementById("word08").innerHTML = document.getElementById("field08").value;
+  document.getElementById("word09").innerHTML = document.getElementById("field09").value;
+  document.getElementById("word10").innerHTML = document.getElementById("field10").value;
+  document.getElementById("word11").innerHTML = document.getElementById("field11").value;
+  document.getElementById("word12").innerHTML = document.getElementById("field12").value;
   document.getElementById("story").style.display = "block";
 }
