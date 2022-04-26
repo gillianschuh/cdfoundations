0d1c8b315da32621300a1d39b8ec07a669521c0f
gillianschuh <schug895@newschool.edu>
2022-04-25 20:57:36 -0400

    Update myscript.js

--- basicjavascript/myscript.js
+++ basicjavascript/myscript.js
@@ -5,6 +5,7 @@ for ( var i = 0; i < cars.length ; i++ ) {
 	console.log(cars[i]);
   console.log(i);
   // Below I ouput the data to the webpage. I take the innerHTML of "jOutput" and add a space and then the value of cars[i] with every iteration of the loop.
+document.getElementById("jOutput").innerHTML = document.getElementById("jOutput").innerHTML + " " + cars[i];
 }
 
 
